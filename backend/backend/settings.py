b8bfb9811438a41ffc3437206170d68cad0ce88c
jass228 <assouma@et.esiea.fr>
2025-01-15 20:54:52 +0100

    allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    os.environ.get("ALLOWED_HOSTS")
+]
 
 # Application definition
 
@@ -96,13 +97,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "https://todo-app-amber-nine.vercel.app",
 ]
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'todo-app-hlja.onrender.com',  # Add your Render domain here
-]
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
